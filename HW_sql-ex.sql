b5ab560a6c8db3ba1107171896c2ba6b816fcb71
svetkhar <firebug.qa@gmail.com>
2023-04-17 17:54:16 +1000

    sql hw 1

--- HW_sql-ex.sql
+++ HW_sql-ex.sql
@@ -1,2 +1,5 @@
 1. Найдите номер модели, скорость и размер жесткого диска для всех ПК стоимостью менее 500 дол. Вывести: model, speed и hd
 SELECT model, speed, hd FROM Pc WHERE Price < 500 
+
+2. Найдите производителей принтеров. Вывести: maker 
+SELECT DISTINCT Maker FROM PRODUCT WHERE type = 'Printer'
