6d2694b54bfb895741c5e3198db3c75add0344d3
Svetlana Kharitonova <126975306+svetkhar@users.noreply.github.com>
2023-04-18 21:22:19 +1000

    Update HW_sql-ex.sql

--- HW_sql-ex.sql
+++ HW_sql-ex.sql
@@ -3,14 +3,6 @@ SELECT model, speed, hd FROM Pc WHERE Price < 500
 
 2. Найдите производителей принтеров. Вывести: maker 
 SELECT DISTINCT Maker FROM PRODUCT WHERE type = 'Printer'
-3.Найдите номер модели, объем памяти и размеры экранов ПК-блокнотов, цена которых превышает 1000 дол.
-SELECT model, ram, screen FROM Laptop WHERE Price > 1000
-
-4. Найдите все записи таблицы Printer для цветных принтеров.
-SELECT * FROM Printer WHERE Color = 'y'
-
-5. Найдите номер модели, скорость и размер жесткого диска ПК, имеющих 12x или 24x CD и цену менее 600 дол. 
-SELECT model, speed, hd FROM Pc WHERE (cd = '12x' OR cd = '24x') AND Price < 600
 
 3.Найдите номер модели, объем памяти и размеры экранов ПК-блокнотов, цена которых превышает 1000 дол.
 SELECT model, ram, screen FROM Laptop WHERE Price > 1000
